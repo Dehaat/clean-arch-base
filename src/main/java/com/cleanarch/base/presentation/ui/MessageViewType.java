c670d4a15ea705eca7cdd364643ce771e54f28b2
Vishal <vishal.sharma@agrevolution.in>
2024-08-28 12:18:26 +0530

    [ERP-10367] Upgrade Android 14 & React Native 0.74.4

--- src/main/java/com/cleanarch/base/presentation/ui/MessageViewType.java
+++ src/main/java/com/cleanarch/base/presentation/ui/MessageViewType.java
@@ -1,10 +1,12 @@
 package com.cleanarch.base.presentation.ui;
 
 import androidx.annotation.IntDef;
+
+import kotlin.annotation.AnnotationRetention;
 import kotlin.annotation.Retention;
 
 @IntDef({MessageViewType.TOAST, MessageViewType.DIALOG})
-@Retention()
+@Retention(AnnotationRetention.SOURCE)
 public @interface MessageViewType {
     int TOAST = 0;
     int DIALOG = 1;
