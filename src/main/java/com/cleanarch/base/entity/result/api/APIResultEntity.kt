f314ddec60d9931fde99f3399ae6afb034694326
shivam21 <shivam.bhusri@agrevolution.in>
2025-06-05 11:58:56 +0530

    Add app trace Id

--- src/main/java/com/cleanarch/base/entity/result/api/APIResultEntity.kt
+++ src/main/java/com/cleanarch/base/entity/result/api/APIResultEntity.kt
@@ -3,16 +3,18 @@ package com.cleanarch.base.entity.result.api
 import com.cleanarch.base.common.ApiExtraInfo
 import com.cleanarch.base.entity.result.IResultEntity
 
-sealed class APIResultEntity<out R> : IResultEntity {
-    data class Success<out T>(val data: T) : APIResultEntity<T>()
-    sealed class Failure : APIResultEntity<Nothing>() {
-        data class ErrorException(val exceptionError: Exception, val apiExtraInfo: ApiExtraInfo?) : Failure()
-        data class ErrorFailure(
+sealed class APIResultEntity<out R>(val apiTraceId: String?) : IResultEntity {
+    class Success<out T>(val data: T, apiTraceId: String? = "") : APIResultEntity<T>(apiTraceId)
+    sealed class Failure(apiTraceId: String?) : APIResultEntity<Nothing>(apiTraceId) {
+        class ErrorException(val exceptionError: Exception, val apiExtraInfo: ApiExtraInfo?) :
+            Failure(apiExtraInfo?.get("IB-Request-Identifier"))
+
+        class ErrorFailure(
             val httpCode: Int,
             val responseMessage: String,
             val responseErrorBody: String?,
             val apiExtraInfo: ApiExtraInfo?,
             val tmsErrorCode: String? = null
-        ) : Failure()
+        ) : Failure(apiExtraInfo?.get("IB-Request-Identifier"))
     }
 }
